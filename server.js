36a5a8c220e9119fb5090d389b4e9b1849e12450
Joscelyn <JoscelynRose@gmail.com>
2019-09-03 16:56:06 -0400

    now can delete a post

--- server.js
+++ server.js
@@ -31,21 +31,73 @@ server.get('/api/posts', (req, res) => {
 
 // get a specific post
 server.get('/api/posts/:id', (req, res) => {
-  const postId = req.params.id;
-  Posts.findById(postId)
+  const { id } = req.params;
+  Posts.findById(id)
     .then(post => {
-      if (!post) {
+      if (post) {
+        res.status(200).json(post);
+      } else {
         res
           .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' });
-      } else {
-        res.status(200).json(post);
+          .json({ error: 'The post with the specified ID does not exist' });
       }
     })
     .catch(error => {
       res
         .status(500)
         .json({ message: 'The post information could not be retrieved.' });
+    });
+});
+
+// post a new post
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ error: 'Please provide the title and contents for the post' });
+  }
+
+  Posts.insert({ title, contents })
+    .then(({ id }) => {
+      Posts.findById(id)
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err => {
+          res.status(500).json({
+            error:
+              'There was an error while retrieving the post from the database'
+          });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      });
+    });
+});
+
+// delete a post
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.body;
+
+  Posts.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ error: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'The post could not be removed'
+      });
     });
 });
 module.exports = server; // CommonJS modules (node)
